5570c468954ecee9b6dfcbb1ad3ca7aa3b9fbc4f
jai <jk007rai@gmail.com>
2023-06-04 13:08:13 +0530

    Initial commit

---
+++ web.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    # st.session_state is a dictionary type which is used to
+    # append new items in the todos
+
+    functions.write_todos(todos)
+    # calls the write_todos from function.py which edits the new_todo
+
+# steps to create web page
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app")
+st.write("This app is to increase your productivity")
+
+# creates checkboxes for todos list
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+# to create new input box
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
+
